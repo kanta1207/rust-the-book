db858c7f7909594b9958c95692c92baa2f5c568f
kanta1207 <kan0712livepool@gmail.com>
2025-10-09 16:50:30 -0700

    derive and use struct more

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -9,10 +9,14 @@ fn main() {
  println!("user_taro email : {}", user_taro.email);     
  println!("user_taro sign_in_count : {}", user_taro.sign_in_count);
  println!("user_taro active : {}", user_taro.active);
+ debug_user(user_taro);
+ rectangle();
+ rectangle_with_tuple();
+ rectangle_with_struct();
 }
 
 
-
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
@@ -30,4 +34,41 @@ fn build_user(username: String, email: String) -> User {
 user
 }
 
+fn debug_user(user: User) {
+    println!("user: {:?}", user);
+}
+
+fn rectangle(){
+    let width = 30;
+    let height = 50;
+    println!("The area of the rectangle is {} square pixels.", area(width, height));
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn rectangle_with_tuple(){
+    let rect = (30, 50);
+    println!("The area of the rectangle with tuple is {} square pixels.", area_with_tuple(rect));
+}
+
+fn area_with_tuple(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
+}
+
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area_with_struct(rect: Rectangle) -> u32 {
+    rect.width * rect.height
+}
+
+fn rectangle_with_struct(){
+    let rect = Rectangle { width: 30, height: 50 };
+    println!("The area of the rectangle with struct is {} square pixels.", area_with_struct(rect));
+}
 
