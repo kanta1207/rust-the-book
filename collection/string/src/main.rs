d77f6c90e8da5344dbe895604a27d0a436232e9a
kanta1207 <kan0712livepool@gmail.com>
2025-10-14 18:37:08 -0700

    string (minimum)

---
+++ collection/string/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    fn_string();
+    extend_string(String::from("Good Morning"));
+}
+
+
+fn fn_string() {
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+}
+
+fn extend_string(s : String) {
+    s.push_str(", world!");
+}
