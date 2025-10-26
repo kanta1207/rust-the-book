095a48a27793d70269d961a39414d5e9d59a41b7
kanta1207 <kan0712livepool@gmail.com>
2025-10-26 04:09:54 -0700

    closure cache struct

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -11,29 +11,61 @@ fn main() {
     );
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+    last_arg: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+            last_arg: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match (self.last_arg, self.value) {
+            (Some(a), Some(v)) if a == arg => v,
+            _ => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                self.last_arg = Some(arg);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+    
     if intensity < 25 {
-
         println!(
-            // 今日は{}回腕立て伏せをしてください！
             "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
-
         println!(
-            // 次に、{}回腹筋をしてください！
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
-            // 今日は休憩してください！水分補給を忘れずに！
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
-                // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
@@ -41,9 +73,20 @@ fn generate_workout(intensity: u32, random_number: u32) {
 
 
 
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
 }
 
